bb6b929f41a4e5c7ecf064270f4b64520b089184
daorozc <daorozc@bancolombia.com.co>
2023-08-22 21:18:57 -0500

    docs(wikipediaLoader.py): add file-level docstring to explain the purpose of the file refactor(wikipediaLoader.py): reformat WikipediaLoader instantiation for better readability fix(wikipediaLoader.py): change load_max_docs from 2 to 1 to load only one document feat(wikipediaLoader.py): print the content of the loaded page instead of its length for more useful output

--- wikipediaLoader.py
+++ wikipediaLoader.py
@@ -1,6 +1,14 @@
+"""
+This file contains an example usage of the WikipediaLoader class from the langchain package.
+It loads the content of the Wikipedia page for "X" in X language and prints it to the console.
+"""
+
 from langchain.document_loaders import WikipediaLoader
 
-docs = WikipediaLoader(query="Colombia", lang="es", load_max_docs=2).load()
-print(len(docs))
+docs = WikipediaLoader(
+    query="Colombia",
+    lang="es",
+    load_max_docs=1,
+).load()
 
 print(docs[0].page_content)
